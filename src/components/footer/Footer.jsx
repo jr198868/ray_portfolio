88f935df28397097e012abe4da27f6df355db38a
jr198868 <rjingraymond88@gmail.com>
2022-03-01 22:25:56 -0500

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => (
+  <div className = "footer">
+    <p className = "footer-text" >Copyright © 2022 Raymond Jing.</p>
+  </div>
+);
+
+export default Footer;

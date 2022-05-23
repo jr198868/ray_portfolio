e4ac9eda65f7c993da14a10441f0cf13f33677af
Raymond <rjingraymond88@gmail.com>
2022-05-23 00:32:28 -0400

    change the code sample size

--- src/components/codesample/Codesample.jsx
+++ src/components/codesample/Codesample.jsx
@@ -9,10 +9,10 @@ export default function Codesample() {
       <div className = 'ray__aboutme_biography'> 
         Code Demo 1
       </div>
-      <iframe src="https://codesandbox.io/embed/bitter-sun-gnl2rd?fontsize=14&hidenavigation=1&theme=dark"
+      <iframe src="https://codesandbox.io/embed/suspicious-swirles-12723n?fontsize=14&hidenavigation=1&theme=dark"
           
           className = "ray_sandbox"
-          style = {{width: "85%", height: 550}}
+          style = {{width: "85%", height: 720}}
           title = "bitter-sun-gnl2rd"
           allow = "accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
           sandbox = "allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"

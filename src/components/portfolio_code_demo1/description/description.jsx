78d77d7cf91a40871d504d7fda4fdbba1fb7f8ef
Raymond <rjingraymond88@gmail.com>
2022-06-01 12:10:54 -0400

    add portfolio_code_demo1 more feathers

---
+++ src/components/portfolio_code_demo1/description/description.jsx
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import './description.css'
+
+  
+  export default function Description() {
+      return (
+          <div>
+            <br />
+                <div className ='description'>
+                    An arithmetic progression is a sequence of numbers that the <br />
+                    difference between each two items is constant. Type initial term, <br />
+                    common difference, and the number of term to virtualize arithmetic progression <br />
+                    (e.g., The initial term = 1, The common difference = 0.2, and The number of term = 20)
+                </div>
+            <br />
+          </div>
+      )
+
+  }
---
+++ src/components/portfolio_code_demo1/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+
+
+function Header() {
+    return (
+      <div className = 'header_fibonacci'>
+          Virtualize Arithmetic Progression
+      </div>
+    );
+  }
+  
+  export default Header;
